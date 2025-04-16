b2b93485f37840ca4aaa9ff376d21f25e5639954
Jonathan Palencia <ciachn@gmail.com>
2025-04-16 01:29:16 -0600

    Completed form field classes

--- dev/main.jsx
+++ dev/main.jsx
@@ -2,12 +2,7 @@
 import { Router } from 'riza';
 import "./main.css";
 
-import "../src/root.css";
-import "../src/common.css";
-import "../src/dialog.css";
-import "../src/btn-strip.css";
-import "../src/btn.css";
-import "../src/form.css";
+import "../src/main.css";
 
 const App = () =>
 <r-panel data-root="true" class="p-4 s-fill" onCreated={ () => queueMicrotask(() => Router.refresh()) }>
@@ -15,73 +10,108 @@ const App = () =>
     <button class="btn" onClick={ () => window.document.documentElement.classList.toggle('light-mode') }>Toggle Light/Dark</button>
     <button class="btn" onClick={ (ev) => ev.currentTarget.querySelectorParent('[data-root]').dialog.toggleVisibility() }>Toggle Dialog</button>
 
-    <hr/>
-    <p><b>Buttons</b></p>
-    <div class="mb-1">
-        <button class="btn">btn</button>
-        <button class="btn outline">btn outline</button>
-        <button class="btn small">btn small</button>
-        <button class="btn small outline">btn small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn primary">btn primary</button>
-        <button class="btn primary outline">btn primary outline</button>
-        <button class="btn primary small">btn primary small</button>
-        <button class="btn primary small outline">btn primary small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn secondary">btn secondary</button>
-        <button class="btn secondary outline">btn secondary outline</button>
-        <button class="btn secondary small">btn secondary small</button>
-        <button class="btn secondary small outline">btn secondary small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn success">btn success</button>
-        <button class="btn success outline">btn success outline</button>
-        <button class="btn success small">btn success small</button>
-        <button class="btn success small outline">btn success small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn warn">btn warn</button>
-        <button class="btn warn outline">btn warn outline</button>
-        <button class="btn warn small">btn warn small</button>
-        <button class="btn warn small outline">btn warn small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn danger">btn danger</button>
-        <button class="btn danger outline">btn danger outline</button>
-        <button class="btn danger small">btn danger small</button>
-        <button class="btn danger small outline">btn danger small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn info">btn info</button>
-        <button class="btn info outline">btn info outline</button>
-        <button class="btn info small">btn info small</button>
-        <button class="btn info small outline">btn info small outline</button>
-    </div>
-    <div class="mb-1">
-        <button class="btn inv">btn inv</button>
-        <button class="btn inv outline">btn inv outline</button>
-        <button class="btn inv small">btn inv small</button>
-        <button class="btn inv small outline">btn inv small outline</button>
+    <div class="f-row">
+
+        <div className="s-6">
+            <p><b>Buttons</b></p>
+            <div class="mb-1">
+                <button class="btn">btn</button>
+                <button class="btn outline">btn outline</button>
+                <button class="btn s-small">btn s-small</button>
+                <button class="btn outline s-small">btn outline s-small</button>
+                <button class="btn s-tiny">btn tiny</button>
+                <button class="btn outline s-tiny">btn outline s-tiny</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn primary">btn primary</button>
+                <button class="btn primary outline">btn primary outline</button>
+                <button class="btn primary s-small">btn primary s-small</button>
+                <button class="btn primary s-small outline">btn primary s-small outline</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn secondary">btn secondary</button>
+                <button class="btn secondary outline">btn secondary outline</button>
+                <button class="btn secondary s-small">btn secondary s-small</button>
+                <button class="btn secondary s-small outline">btn secondary s-small outline</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn success">btn success</button>
+                <button class="btn success outline">btn success outline</button>
+                <button class="btn success s-small">btn success s-small</button>
+                <button class="btn success s-small outline">btn success s-small outline</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn warn">btn warn</button>
+                <button class="btn warn outline">btn warn outline</button>
+                <button class="btn warn s-small">btn warn s-small</button>
+                <button class="btn warn s-small outline">btn warn s-small outline</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn danger">btn danger</button>
+                <button class="btn danger outline">btn danger outline</button>
+                <button class="btn danger s-small">btn danger s-small</button>
+                <button class="btn danger s-small outline">btn danger s-small outline</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn info">btn info</button>
+                <button class="btn info outline">btn info outline</button>
+                <button class="btn info s-small">btn info s-small</button>
+                <button class="btn info s-small outline">btn info s-small outline</button>
+            </div>
+            <div class="mb-1">
+                <button class="btn inv">btn inv</button>
+                <button class="btn inv outline">btn inv outline</button>
+                <button class="btn inv s-small">btn inv s-small</button>
+                <button class="btn inv s-small outline">btn inv s-small outline</button>
+            </div>
+        </div>
+
+        <div className="s-6">
+            <p><b>Form Elements</b></p>
+            <div class="f-row cols-2 g-4">
+                <div class="s-6">
+                    <div class="form-field">
+                        <label>Full Name</label>
+                        <input type="text" />
+                        <span class="field-error">Field error message.</span>
+                    </div>
+                    <div class="form-field">
+                        <label>Description</label>
+                        <textarea></textarea>
+                        <small>This is a small note.</small>
+                    </div>
+                    <div class="form-field">
+                        <label>Select Country</label>
+                        <select>
+                            <option>USA</option>
+                            <option>Honduras</option>
+                            <option>Japan</option>
+                        </select>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 
-    <hr/>
-    <p><b>Form Elements</b></p>
+    <r-panel class="dialog right">
+        <div class="title text-center py-3 fs-1 fw-600">
+            <b>Form Example</b>
+        </div>
 
-    <div class="f-row g-2">
-        <div class="s-3">
-            <div class="form-group">
+        <div class="content px-4 pb-3 fs-2d" style:width="350px">
+            <div class="form-field">
                 <label>Full Name</label>
                 <input type="text" />
+                <span class="field-error">Field error message.</span>
             </div>
-
-            <div class="form-group">
+            <div class="form-field">
                 <label>Description</label>
                 <textarea></textarea>
+                <small>This is a small note.</small>
             </div>
-
-            <div class="form-group">
+            <div class="form-field">
                 <label>Select Country</label>
                 <select>
                     <option>USA</option>
@@ -90,30 +120,9 @@ const App = () =>
                 </select>
             </div>
         </div>
-
-        <div class="s-3">
-            <div class="form-group">
-                <label>Full Name</label>
-                <input type="text" />
-            </div>
-
-            <div class="form-group">
-                <label>Description</label>
-                <textarea></textarea>
-            </div>
-
-            <div class="form-group">
-                <label>Select Country</label>
-                <select>
-                    <option>USA</option>
-                    <option>Honduras</option>
-                    <option>Japan</option>
-                </select>
-            </div>
-        </div>
-    </div>
-
-    <r-panel class="dialog center" data-ref="dialog">
+    </r-panel>
+    
+    <r-panel class="dialog center is-inactive" data-ref="dialog">
         <div class="title text-center py-3 fs-1 fw-600">
             <b>Confirmation</b>
         </div>
